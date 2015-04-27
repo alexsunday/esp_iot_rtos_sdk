d9f5204b4f4a5ffd65bf8f47b2d0edbf83f78251
sunchengcheng <sun@chengcheng.info>
2015-04-27 17:56:32 +0800

    可连接网络 可开放wifi 可选择性的做以上事情了。

--- app/user/user_main.c
+++ app/user/user_main.c
@@ -15,15 +15,20 @@
 
 #include "udhcp/dhcpd.h"
 #include "lwip/inet.h"
+#include "lwip/sockets.h"
 
 #include "gpio.h"
 
 #define FLASH_HEAD_ADDR 0x3C000
 #define DEFAULT_SSID "iPLUG"
 #define DEFAULT_SSID_PWD "iPLUG123"
-#define DEFAULT_GWADDR "192.168.10.1"
-#define DHCP_BEGIN_ADDR "192.168.10.101"
-#define DHCP_END_ADDR "192.168.10.108"
+#define DEFAULT_GWADDR "192.168.10.253"
+#define DHCP_BEGIN_ADDR "192.168.10.100"
+#define DHCP_END_ADDR "192.168.10.110"
+#define LOCAL_SERVER_PORT 8091
+#define CLOUD_SERVER "211.155.86.145"
+#define CLOUD_PORT 10090
+
 
 enum DEV_TYPE {
 	DEV_UNKNOWN, DEV_PLUG
@@ -44,63 +49,6 @@ typedef struct _rw_info{
 	uint8 dev_type;
 } rw_info;
 
-/******************************************************************************
- * FunctionName : user_init
- * Description  : entry of user application, init user function here
- * Parameters   : none
- * Returns      : none
- *******************************************************************************/
-void ICACHE_FLASH_ATTR user_init_(void)
-{
-	uart_init_new();
-
-	/* need to set opmode before you set config */
-	wifi_set_opmode(STATIONAP_MODE);
-
-	{
-		struct station_config *config = (struct station_config *) zalloc(
-				sizeof(struct station_config));
-		sprintf(config->ssid, "ChinaNet-966");
-		sprintf(config->password, "Jchen0406");
-
-		/* need to sure that you are in station mode first,
-		 * otherwise it will be failed. */
-		wifi_station_set_config(config);
-		free(config);
-	}
-
-	{
-		struct ip_info ipinfo;
-
-		ipinfo.gw.addr = ipaddr_addr("192.168.145.253");
-		ipinfo.ip.addr = ipaddr_addr("192.168.145.253");
-		ipinfo.netmask.addr = ipaddr_addr("255.255.255.0");
-
-		wifi_set_ip_info(SOFTAP_IF, &ipinfo);
-	}
-
-//    {
-//        struct dhcp_info *pdhcp_info = NULL;
-//
-//        pdhcp_info = (struct dhcp_info *)zalloc(sizeof(struct dhcp_info));
-//        pdhcp_info->start_ip = ipaddr_addr("192.168.145.100");
-//        pdhcp_info->end_ip = ipaddr_addr("192.168.145.110");    // don't set the range too large, because it will cost memory.
-//        pdhcp_info->max_leases = 10;
-//        pdhcp_info->auto_time = 60;
-//        pdhcp_info->decline_time = 60;
-//        pdhcp_info->conflict_time = 60;
-//        pdhcp_info->offer_time = 60;
-//        pdhcp_info->min_lease_sec = 60;
-//        dhcp_set_info(pdhcp_info);
-//        free(pdhcp_info);
-//    }
-//
-//    udhcpd_start();
-
-//xTaskCreate(test_select, "test_select", 256, NULL, 2, NULL);
-//xTaskCreate(task2, "tsk2", 256, NULL, 2, NULL);
-//xTaskCreate(task3, "tsk3", 256, NULL, 2, NULL);
-}
 
 void raw_show(unsigned char* buf, size_t buflen)
 {
@@ -128,6 +76,7 @@ void show_rw(rw_info* rw)
 	printf("Router SSID PWD: [%s]\n", rw->ssid_pwd);
 }
 
+
 void show_sysinfo()
 {
 	uint32 chipid = system_get_chip_id();
@@ -147,16 +96,19 @@ void show_sysinfo()
 	printf("==========SYS INFO==========\n");
 }
 
-void spi_flash_write_test()
+
+void spi_flash_write_test(enum RUN_MODE mode)
 {
 	rw_info rw;
 	memset(&rw, 0, sizeof(rw));
-	rw.server_addr = inet_addr("192.168.10.1");
+	rw.server_addr = inet_addr("192.168.0.241");
 	rw.server_port = 10010;
-	strcpy(rw.ssid, "ChinaNet-966");
-	strcpy(rw.ssid_pwd, "Jchen0406");
+	strcpy(rw.ssid, "useease2");
+	strcpy(rw.ssid_pwd, "1CBE991A14");
 	strcpy(rw.ssid_mine, "iPLUG");
 	strcpy(rw.ssid_pwd_mine, "iPLUG123");
+	rw.run_mode = mode;
+	rw.dev_type = DEV_PLUG;
 
 	show_rw(&rw);
 
@@ -176,6 +128,7 @@ void spi_flash_write_test()
 	}
 }
 
+
 void spi_flash_read_test()
 {
 	rw_info rw;
@@ -190,145 +143,146 @@ void spi_flash_read_test()
 	}
 }
 
+
 void gpio_test(void* param)
 {
-	int pin = BIT13;
-	//PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
+	int pin = 0;
+
+	rw_info* rw = (rw_info*)param;
+
+	if(rw->run_mode == CLIENT_ONLY) {
+		pin = BIT12;
+	} else if(rw->run_mode == WIFI_BOARDCAST) {
+		pin = BIT13;
+	} else {
+		pin = BIT14;
+	}
+
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
 	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
-	printf("========================GPIO TEST========================\n");
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U,FUNC_GPIO14);
+
 	while (1) {
-		printf("LED UP\n");
 		gpio_output_set(pin, 0, pin, 0);
 		vTaskDelay(1000 / portTICK_RATE_MS);
-		printf("LED DOWN\n");
 		gpio_output_set(0, pin, pin, 0);
 		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
-	printf("========================GPIO TEST========================\n");
 }
 
 
-void local_server(void* param)
+void client_mode(void* param)
 {
-	while(1)
-	{
+	printf("CLIENT MODE\n");
+
+	while(1) {
+		int recvbytes, sin_size, str_len, sta_socket, iRet;
+		struct sockaddr_in local_ip, remote_ip;
+
+		sta_socket = socket(AF_INET, SOCK_STREAM, 0);
+		if(-1 == sta_socket) {
+			printf("sock init error\n");
+			close(sta_socket);
+			continue;
+		}
+		printf("client socket init ok.\n");
+
+		memset(&remote_ip, 0, sizeof(struct sockaddr_in));
+		remote_ip.sin_family = AF_INET;
+		remote_ip.sin_addr.s_addr = inet_addr(CLOUD_SERVER);
+		remote_ip.sin_port = htons(CLOUD_PORT);
+
+		iRet = connect(sta_socket, (struct sockaddr*)&remote_ip, sizeof(struct sockaddr));
+		if(iRet != 0) {
+			printf("Connect fail\n");
+			close(sta_socket);
+			vTaskDelay(5000 / portTICK_RATE_MS);
+			continue;
+		}
+		printf("Connected CLOUD :[%s:%d]\n", CLOUD_SERVER, CLOUD_PORT);
+		char* recv_buf = (char*)zalloc(128);
+		while((recvbytes = read(sta_socket, recv_buf, 128)) > 0) {
+			recv_buf[recvbytes] = 0;
+			printf("Data from CLIENT: [%d], [%s]\n", recvbytes, recv_buf);
+		}
+		free(recv_buf);
+
+		if(recvbytes <= 0) {
+			printf("disconnected...\n");
+		}
+
+		vTaskDelay(10 / portTICK_RATE_MS);
+	}
+
+	vTaskDelete(NULL);
+}
+
+
+void wifi_mode(void* param)
+{
+	while(1) {
+		struct sockaddr_in server_addr, client_addr;
+		int server_sock, client_sock, iRet;
+		socklen_t sin_size;
+
+		memset(&server_addr, 0, sizeof(struct sockaddr_in));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr.s_addr = INADDR_ANY;
+		server_addr.sin_port = htons(LOCAL_SERVER_PORT);
+		int recvbytes;
+
+		do {
+			server_sock = socket(AF_INET, SOCK_STREAM, 0);
+			if(server_sock == -1) {
+				printf("socket error\n");
+				break;
+			}
+			printf("init socket ok\n");
+
+			iRet = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
+			if(iRet == -1) {
+				printf("BIND ERROR\n");
+				break;
+			}
+			printf("socket bind ok\n");
+
+			iRet = listen(server_sock, 2);
+			if(iRet == -1) {
+				printf("listen error\n");
+				break;
+			}
+			printf("socket listen ok");
+			sin_size = sizeof(client_addr);
+
+			for(;;) {
+				client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &sin_size);
+				if(client_sock < 0) {
+					printf("accept error\n");
+					continue;
+				}
+
+				printf("New connection from [%s:%d]\n", inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port));
+				char* recv_buf = (char*)zalloc(128);
+				while((recvbytes = read(client_sock, recv_buf, 128)) > 0) {
+					recv_buf[recvbytes] = 0;
+					printf("RECV: [%d], [%s]\n", recvbytes, recv_buf);
+				}
+				free(recv_buf);
+
+				if(recvbytes <= 0) {
+					printf("disconnected...\n");
+					close(client_sock);
+				}
+			}
+
+		} while(0);
+
 		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
+
+	vTaskDelete(NULL);
 }
 
-
-uint8 wifi_boardcast_mode(rw_info* rw)
-{
-	if (!wifi_set_opmode_current(SOFTAP_MODE)) {
-		printf("[%s] [%s] ERROR\n", "wifi_set_opmode", __func__);
-		return -1;
-	}
-
-	{
-		struct softap_config apconfig;
-		memset(&apconfig, 0, sizeof(struct softap_config));
-		strcpy(apconfig.ssid, DEFAULT_SSID);
-		strcpy(apconfig.password, DEFAULT_SSID_PWD);
-		apconfig.ssid_len = 0;
-		apconfig.authmode = AUTH_WPA_WPA2_PSK;
-		apconfig.ssid_hidden = 0;
-		apconfig.max_connection = 5;
-		apconfig.beacon_interval = 100;
-
-		if (!wifi_softap_set_config_current(&apconfig)) {
-			printf("[%s] [%s] ERROR\n", __func__,
-					"wifi_softap_set_config_current");
-			return -1;
-		}
-	}
-
-	{
-		struct ip_info ipinfo;
-
-		ipinfo.gw.addr = ipaddr_addr(DEFAULT_GWADDR);
-		ipinfo.ip.addr = ipaddr_addr(DEFAULT_GWADDR);
-		ipinfo.netmask.addr = ipaddr_addr("255.255.255.0");
-
-		if(!wifi_set_ip_info(SOFTAP_IF, &ipinfo)) {
-			printf("[%s] [%s] ERROR\n", __func__, "wifi_set_ip_info ap_if");
-			return -1;
-		}
-	}
-
-	{
-		struct dhcp_info dhcpinfo;
-
-		dhcpinfo.start_ip = ipaddr_addr(DHCP_BEGIN_ADDR);
-		dhcpinfo.end_ip = ipaddr_addr(DHCP_END_ADDR);
-		dhcpinfo.max_leases = 10;
-		dhcpinfo.auto_time = 60;
-		dhcpinfo.decline_time = 60;
-		dhcpinfo.conflict_time = 60;
-		dhcpinfo.offer_time = 60;
-		dhcpinfo.min_lease_sec = 60;
-		dhcp_set_info(&dhcpinfo);
-		udhcpd_start();
-	}
-
-	xTaskCreate(local_server, "local_server", 256, NULL, 0, NULL);
-
-	return 0;
-}
-
-uint8 client_only_mode(rw_info* rw)
-{
-	if (!wifi_set_opmode_current(STATION_MODE)) {
-		printf("[%s] [%s] ERROR\n", "wifi_set_opmode", __func__);
-		return -1;
-	}
-
-	{
-		//连接AP
-		struct station_config config;
-		struct ip_info ipinfo;
-		bool status;
-
-		strcpy(config.ssid, rw->ssid);
-		strcpy(config.password, rw->ssid_pwd);
-		status = wifi_station_set_config_current(&config);
-		if (!status) {
-			printf("[%s] [%s] Config fail\n", rw->ssid, rw->ssid_pwd);
-			return -1;
-		}
-
-		if (!wifi_station_connect()) {
-			printf("[%s] WIFI CONNECT AP ERROR\n", rw->ssid);
-			return -1;
-		} else {
-			printf("[%s] WIFI CONNECTED!\n", rw->ssid);
-		}
-
-		//连接成功后，开启DHCPC
-		if (!wifi_station_dhcpc_start()) {
-			printf("[%s] DHCPC start ERROR\n", rw->ssid);
-			return -1;
-		} else {
-			printf("[%s] DHCP CLIENT START OK\n", rw->ssid);
-		}
-
-		//显示IP地址
-		if (!wifi_get_ip_info(STATION_IF, &ipinfo)) {
-			printf("[%s] [%s] ERROR\n", rw->ssid, __func__);
-			return -1;
-		}
-
-		printf("IP ADDR: [%s]\n", inet_ntoa(ipinfo.ip.addr));
-		printf("NETMASK: [%s]\n", inet_ntoa(ipinfo.netmask.addr));
-		printf("GW ADDR: [%s]\n", inet_ntoa(ipinfo.gw.addr));
-	}
-
-	{
-		//连接云端
-		printf("ALL OK, prepare to connect CLOUD\n");
-	}
-
-	return 0;
-}
 
 void ICACHE_FLASH_ATTR user_init(void)
 {
@@ -336,9 +290,8 @@ void ICACHE_FLASH_ATTR user_init(void)
 	rw_info rw;
 
 	uart_init_new();
-	vTaskDelay(5000 / portTICK_RATE_MS);
-
-	show_sysinfo();
+	//spi_flash_write_test(WIFI_BOARDCAST);
+    printf("SDK version:%s\n", system_get_sdk_version());
 
 	if (spi_flash_read(FLASH_HEAD_ADDR, (uint32*) &rw, sizeof(rw))
 			!= SPI_FLASH_RESULT_OK) {
@@ -349,11 +302,65 @@ void ICACHE_FLASH_ATTR user_init(void)
 		iRet = 0;
 	}
 
-	if (iRet == 0 && rw.run_mode == CLIENT_ONLY) {
-		printf("CLIENT MODE\n");
-		client_only_mode(&rw);
-	} else {
-		printf("WIFI BOARDCAST MODE\n");
-		wifi_boardcast_mode(&rw);
+	/* need to set opmode before you set config */
+
+	if(rw.run_mode == CLIENT_ONLY)
+    {
+		wifi_set_opmode(STATION_MODE);
+        struct station_config *config = (struct station_config *)zalloc(sizeof(struct station_config));
+        sprintf(config->ssid, "useease2");
+        sprintf(config->password, "1CBE991A14");
+
+        /* need to sure that you are in station mode first,
+         * otherwise it will be failed. */
+        wifi_station_set_config(config);
+        free(config);
+        xTaskCreate(client_mode, "client_mode", 256, NULL, 0, NULL);
+    }
+	else
+	{
+		wifi_set_opmode(SOFTAP_MODE);
+		struct softap_config apconfig;
+		memset(&apconfig, 0, sizeof(struct softap_config));
+		strcpy(apconfig.ssid, DEFAULT_SSID);
+		strcpy(apconfig.password, DEFAULT_SSID_PWD);
+		apconfig.ssid_len = 0;
+		apconfig.authmode = AUTH_WPA_WPA2_PSK;
+		apconfig.ssid_hidden = 0;
+		apconfig.max_connection = 5;
+		apconfig.beacon_interval = 100;
+
+		if (!wifi_softap_set_config(&apconfig)) {
+			printf("[%s] [%s] ERROR\n", __func__,
+					"wifi_softap_set_config");
+		}
+		printf("wifi_softap_set_config success\n");
+
+		struct ip_info ipinfo;
+
+        ipinfo.gw.addr = ipaddr_addr(DEFAULT_GWADDR);
+    	ipinfo.ip.addr = ipaddr_addr(DEFAULT_GWADDR);
+    	ipinfo.netmask.addr = ipaddr_addr("255.255.255.0");
+
+    	wifi_set_ip_info(SOFTAP_IF, &ipinfo);
+
+    	struct dhcp_info *pdhcp_info = NULL;
+
+        pdhcp_info = (struct dhcp_info *)zalloc(sizeof(struct dhcp_info));
+        pdhcp_info->start_ip = ipaddr_addr(DHCP_BEGIN_ADDR);
+        pdhcp_info->end_ip = ipaddr_addr(DHCP_END_ADDR);    // don't set the range too large, because it will cost memory.
+        pdhcp_info->max_leases = 10;
+        pdhcp_info->auto_time = 60;
+        pdhcp_info->decline_time = 60;
+        pdhcp_info->conflict_time = 60;
+        pdhcp_info->offer_time = 60;
+        pdhcp_info->min_lease_sec = 60;
+        dhcp_set_info(pdhcp_info);
+        free(pdhcp_info);
+		udhcpd_start();
+
+		xTaskCreate(wifi_mode, "wifi_mode", 256, NULL, 0, NULL);
 	}
+
+	xTaskCreate(gpio_test, "gpio_led", 256, (void*)&rw, 0, NULL);
 }
